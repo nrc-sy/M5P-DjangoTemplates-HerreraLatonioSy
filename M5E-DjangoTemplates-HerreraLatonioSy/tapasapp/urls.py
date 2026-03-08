3f013f336cac0153b50f9dc96a31ca6c49504b3e
rsy <nathan.riley.sy@student.ateneo.edu>
2026-03-08 22:42:52 +0800

    did part 1 except specific template design

---
+++ M5E-DjangoTemplates-HerreraLatonioSy/tapasapp/urls.py
@@ -0,0 +1,26 @@
+# , ; , ; Nathan Riley Sy, 244311
+# March , 2026 
+
+'''
+I hereby attest to the truth of the following facts:
+
+I have not discussed the Python language code in my program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+I have not used Python language code obtained from another student, or
+any other unauthorized source, either modified or unmodified.
+
+If any Python language code or documentation used in my program was
+obtained from another source, such as a textbook or course notes, that has been clearly noted with proper citation in the
+comments of my program.
+'''
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('basic_list', views.view_basic_list, name='view_basic_list'),
+    path('', views.view_menu, name='view_menu'),
+    path('add_menu', views.add_menu, name='add_menu'),
+]
---
+++ M5E-DjangoTemplates-HerreraLatonioSy/tapasapp/views.py
@@ -0,0 +1,33 @@
+# , ; , ; Nathan Riley Sy, 244311
+# March , 2026 
+
+'''
+I hereby attest to the truth of the following facts:
+
+I have not discussed the Python language code in my program with anyone
+other than my instructor or the teaching assistants assigned to this course.
+
+I have not used Python language code obtained from another student, or
+any other unauthorized source, either modified or unmodified.
+
+If any Python language code or documentation used in my program was
+obtained from another source, such as a textbook or course notes, that has been clearly noted with proper citation in the
+comments of my program.
+'''
+
+from django.shortcuts import render
+from .models import Dish
+
+# Create your views here.
+
+def view_basic_list(request):
+    dish_objects = Dish.objects.all()
+    return render(request, 'tapasapp/basic_list.html', {'dishes':dish_objects})
+
+
+def view_menu(request):
+    dish_objects = Dish.objects.all()
+    return render(request, 'tapasapp/list.html', {'dishes':dish_objects})
+
+def add_menu(request):
+    return render(request, 'tapasapp/add_menu.html')
