2a44725bd195c4bc71f601c30b97ded5d8510d39
ZSL-MSYS22 <zale.sebastian.latonio@student.ateneo.edu>
2026-03-16 19:58:29 +0800

    Tried extending base to view_supplier, view_bottles and add_bottles HTMLs

--- M5P-DjangoTemplates-HerreraLatonioSy/MyInventorySystem/MyInventorySystem/urls.py
+++ M5P-DjangoTemplates-HerreraLatonioSy/MyInventorySystem/MyInventorySystem/urls.py
@@ -1,4 +1,4 @@
-# , ; Zale Sebastian Latonio, ; Nathan Riley Sy, 244311
+# , ; Zale Sebastian Latonio, 242494 ; Nathan Riley Sy, 244311
 # March , 2026 
 
 '''
@@ -37,7 +37,7 @@ from MyInventoryApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('view_supplier/', views.view_supplier, name='view_supplier'),
-    path('view_bottles/', views.view_bottles, name='view_bottles'),
-    path('add_bottle/', views.add_bottle, name='add_bottle'),
+    path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('view_bottles', views.view_bottles, name='view_bottles'),
+    path('add_bottle', views.add_bottle, name='add_bottle'),
 ]
